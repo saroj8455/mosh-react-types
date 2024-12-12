9e1ce199444fa76ec80437f3bee000a975be3b0d
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-12 09:43:33 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1 className='display-1'>Vite + React Mosh React with Typescript</h1>
+
+        <p className='read-the-docs fs-2'>
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
